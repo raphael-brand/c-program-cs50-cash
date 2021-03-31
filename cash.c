4ed6450d94f8a36d554cc893877eae1c6eb3d6c1
Raphael Brand <info@raphaelbrand.de>
2021-03-31 14:14:10 +0000

    request user amount unless it is sero or less

--- cash.c
+++ cash.c
@@ -3,8 +3,12 @@
 
 int main(void)
 {
+// ensure that the user’s input is indeed a floating-point (or integral) value but not that it is non-negative
+    float cash_amount = 0.00;
+    do {
+        cash_amount = get_float("Which amount?");
+    } while(cash_amount <= 0.00);
 
-    float cash_amount = get_float("Which amount?");
     printf("You gave %.2f USD\n", cash_amount);
 }
 
