0ce171ffc5321298fe3a9862a30b2a94d2ccc7a1
Raphael Brand <info@raphaelbrand.de>
2021-04-07 15:50:41 +0000

    fix formatting

--- cash.c
+++ cash.c
@@ -5,12 +5,13 @@ int main(void)
 {
 // ensure that the user’s input is indeed a floating-point (or integral) value but not that it is non-negative
     float cash_amount = 0.00;
-    do {
+    do
+    {
         cash_amount = get_float("Which amount?");
 // If the user fails to provide a non-negative value, your program should re-prompt the user for a valid amount again and again until the user complies
-    } while(cash_amount <= 0.00);
+    }
+    while (cash_amount <= 0.00);
 
     printf("You gave %.2f USD\n", cash_amount);
 }
-
 
